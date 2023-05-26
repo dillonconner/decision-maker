9e3c8b457ddec1e586d15fc592cb543d02ba2cfd
dillonconner <dillonconner@gmail.com>
2023-05-25 22:22:50 -0400

    added vote route

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import UtilBar from "./components/UtilBar";
 import Home from './pages/Home';
 import CreateForm from './components/CreateForm';
 import LoginForm from './components/LoginForm';
+import Vote from './components/Vote';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <Route path='login' element={<LoginForm />} />
             <Route path='' element={<RequireAuth> <Home /> </RequireAuth>} />
             <Route path='new-decision' element={<RequireAuth> <CreateForm /> </RequireAuth>} />
+            <Route path='vote/:decisionId' element={<RequireAuth> <Vote /> </RequireAuth>} />
           </Routes>
           <UtilBar />
         </ProvideAuth>
