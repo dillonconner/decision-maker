624cdefd40227f82c6e4fbf534372f0cd7ed11e8
dillonconner <dillonconner@gmail.com>
2023-05-25 22:22:07 -0400

    added decision slice

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import mapReducer from './reducers/MapSlice';
+import dRequestReducer from './reducers/DRequestSlice';
 
 export default configureStore({
     reducer: {
         map: mapReducer,
+        dRequest: dRequestReducer,
     }
 })
