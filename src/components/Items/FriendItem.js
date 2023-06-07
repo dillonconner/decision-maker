d74fbdeddb7fa226f844ae71af37d175f8177a7a
dillonconner <dillonconner@gmail.com>
2023-06-07 18:54:08 -0400

    added edit prop

--- src/components/Items/FriendItem.js
+++ src/components/Items/FriendItem.js
@@ -1,23 +1,29 @@
 import React from "react";
 import votedCheck from '../../icons/voted-check.png';
+import redX from '../../icons/red-x.png'
 
-const testFriend = {
-    username: 'testuser1',
-    displayname: 'Dillon Conner'
-}
-
-const FriendItem = ({friend, add, remove, added, noIcon=false}) => {
-
-    return (
+const FriendItem = ({friend, edit=false, add, remove, added, noIcon=false}) => {
+    
+    return edit ? (
         <div className="friend-item">
-            {added && !noIcon && <img className="selected" src={votedCheck} alt="selected" />}
-            <h2>{friend.displayname}</h2>
-            
+            <div className="user-info">
+                <h2>{friend.displayname}</h2>
+            </div>
+            <div className="friend-request-buttons">
             {!added ?
-                <button value={friend.user_id} onClick={add}>Add</button>
+                <button className="green" value={friend.user_id} onClick={add}>Add</button>
                 :
-                <button value={friend.user_id} onClick={remove}>Remove</button>
+                <button className="red" value={friend.user_id} onClick={remove}>Remove</button>
             }
+            </div>
+        </div>
+    ) :
+    (
+        <div className="friend-item">
+            {friend.voted && <img className="selected" src={votedCheck} alt="selected" />}
+            <div className="user-info">
+                <h2>{friend.displayname}</h2>
+            </div>
         </div>
     )
 }
