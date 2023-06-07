a6a8e4e17ad8064b0a50b8e29611a44a01bcf66a
dillonconner <dillonconner@gmail.com>
2023-06-07 18:52:49 -0400

    added place and friend items to results

--- src/components/ShowResults.js
+++ src/components/ShowResults.js
@@ -1,22 +1,56 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import PlaceItem from "./Items/PlaceItem";
+import { useDispatch, useSelector } from "react-redux";
+import { selectResultDetails, loadResultDetails } from "../reducers/MapSlice";
+import FriendItem from "./Items/FriendItem";
 
 const ShowResults = ({result, hideResults}) => {
 
-    console.log(result);
+    const dispatch = useDispatch();
+    const resultDetails = useSelector(selectResultDetails);
+    const [addressEncoded, setAddressEncoded] = useState(''); 
+    
+    useEffect(() => {
+        dispatch(loadResultDetails({place_id: result.winner.place.place_id})); 
+    }, []);
+
+    useEffect(() => {
+        if(resultDetails){
+            let buildAddress = resultDetails.formatted_address;
+            buildAddress = buildAddress.replaceAll(', ', '+');
+            buildAddress = buildAddress.replaceAll(' ', '%20');
+            setAddressEncoded(buildAddress);
+        }
+    }, [resultDetails]);
+    
+    
     return (
         <div className="show-results popup">
             <h2 className="popup-header">Results</h2>
             <div className="results-container scrollable">
-                <h2>{result.subject} at {result.time}</h2>
-                <div className="item-section">
+                <h2>{result.subject}</h2>
+                <p>{new Date(result.time).toLocaleString()}</p>
+                
+                <div className="item-section" style={{height: '49%', overflow: 'hidden'}}>
                         <div className="item-section-header">
                             <h3>Winner</h3>
-                            <h3>{result.winner.place.name}</h3>
                         </div>
-                    <p>People: {result.recipients.length}</p>
+                        <div className="places-container">
+                            {resultDetails && <PlaceItem place={resultDetails} full/>}
+                        </div>
+                </div>
+                <div className="item-section">
+                    <div className="item-section-header">
+                        <h3>People: </h3>
+                        <p >{result.recipients.length}</p>
+                    </div>
+                    {result.recipients.map((r, id) => <FriendItem key={id} friend={r} />)}
                 </div>
             </div>
-            <button className="submit-btn" onClick={hideResults}>Done</button>
+            <div className="popup-btns">
+                <a target="_blank" href={`https://www.google.com/maps?saddr=current+location&daddr=${addressEncoded}`}>Get Directions</a>
+                <button onClick={hideResults}>Done</button>
+            </div>
         </div>
     )
 }
