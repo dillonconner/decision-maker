baf43f5c0f412dc8e0723f37f5a2533dc56b9c16
dillonconner <dillonconner@gmail.com>
2023-06-07 18:49:01 -0400

    added voted popup and force decision

--- src/components/Items/DecisionRequestItem.js
+++ src/components/Items/DecisionRequestItem.js
@@ -1,27 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
+import FriendItem from "./FriendItem";
 import votedCheck from '../../icons/voted-check.png';
 import redX from '../../icons/red-x.png'
+import { useAuth } from "../../hooks/use-auth";
+import { baseUrl } from "../../util/apiConfig";
 
-const DecisionRequestItem = ({request, completed, result, onClick, deleteItem}) => {
+const DecisionRequestItem = ({request, completed, result=false, onClick, deleteItem, force}) => {
     
+    const auth = useAuth();
     const voted = request.recipients.filter(element =>  element.voted);
-    //console.log(request);
-    
+    const dispTime = new Date(request.time).toLocaleString();
 
+    const handleForce = (e) => {
+        setVotedPopup(false);
+        force(e, request);
+    }
+
+    const [ votedPopup, setVotedPopup ] = useState(false);
     return (
-        <div className="decision-request-item" onClick={e => onClick(e,request)}>
+        <div className="decision-request-item">
             <div className="item-header">
-                <h3>{request.subject} at {request.time}</h3>
-                
-                
+                <div onClick={e => onClick(e,request)}>
+                    <h3>{request.subject} </h3>
+                    <h6>{dispTime}</h6>
+                </div>
                 {completed && <img className="voted-check" src={votedCheck}  alt="voted" />}
-                {!result && <img className="delete-icon" src={redX} onClick={e => deleteItem(e, request)} /> }
+                {!result && <img className="delete-icon" src={redX} onClick={e => deleteItem(e, request)} alt="remove"/> }
             </div>
             <div className="item-info">
-                <p>From: {request.initiator.display_name}</p>
-                <p>Voted: {voted.length}/{request.recipients.length}</p>
+                <p>From: {request.initiator.displayname}</p>
+                <p style={{'textDecoration': 'underline'}} onClick={e => setVotedPopup(true)}>Voted: {voted.length}/{request.recipients.length}</p>
             </div>
+            
+            {votedPopup &&
+                <div className="friends popup">
+                    <h2 className="popup-header">Participants</h2>
+                    <div className="friends-container">
+                        <div className="scrollable">
+                            {request.recipients.map((f, id) => {
+                                return <FriendItem key={id} friend={f} />
+                            })}
+                        </div>
+                    </div>
+                    <div className="popup-btns">
+                        <button onClick={e => setVotedPopup(false)}>Done</button>
+                        {!result && (request.initiator.user_id === auth.user.user_id) && 
+                            <button onClick={handleForce}>Force Decision</button>
+                        }
+                    </div>
+                </div>
+            }
         </div>
     )
 }
